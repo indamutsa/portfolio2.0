4a2743f46cf39106e7fd9c4423902f72e51a89f6
indamutsa <32111854+Indamutsa@users.noreply.github.com>
2022-10-24 19:52:07 +0200

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The name's Sonny Sangha",
+      "Guy-who-loves-coffee.tsx",
+      "<ButWhoLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src="https://images.unsplash.com/photo-1551270988-87c5ea57cdfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1095&q=80"
+        alt="Picture of the author"
+        width={120}
+        height={120}
+        objectFit={"cover"}
+        className="relative rounded-full mx-auto"
+      />
+      <div className="z-20">
+        <h2 className=" text-sm uppercase text-gray-500 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
